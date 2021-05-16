a3ff3d5b11acb84cddfb7295b69750503b1784f4
Vrajendra VT <Vrajendra.VT@datacore.com>
2021-05-16 23:13:41 +0530

    Add mfc app; Add volume filter

--- FsFilter1/FsFilter1.cpp
+++ FsFilter1/FsFilter1.cpp
@@ -2,6 +2,16 @@
 #include <dontuse.h>
 #include <ntifs.h>
 #include "FsFilter1.h"
+#include "Trace.h"
+#include <stdlib.h>
+#include <ntstrsafe.h>
+
+#define STRING_LEN 128
+
+PDRIVER_OBJECT WppDriverObject = NULL;
+PFLT_PORT port = NULL, ClientPort = NULL;
+UNICODE_STRING drive_filter;
+
 
 const FLT_OPERATION_REGISTRATION Callbacks[] = {
 	{IRP_MJ_CREATE, 0, MiniPreCreate, MiniPostCreate},
@@ -30,7 +40,7 @@ FLT_POSTOP_CALLBACK_STATUS MiniPostCreate(PFLT_CALLBACK_DATA Data, PCFLT_RELATED
 	UNREFERENCED_PARAMETER(CompletionContext);
 	UNREFERENCED_PARAMETER(Flags);
 
-	KdPrint(("Inside Post Create \r\n"));
+	TraceEvent(TRACE_INFO, "Inside Post Create \r\n");
 	return FLT_POSTOP_FINISHED_PROCESSING;
 
 }
@@ -50,7 +60,12 @@ FLT_PREOP_CALLBACK_STATUS MiniPreCreate(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_O
 		status = FltParseFileNameInformation(FileNameInfo);
 		if (NT_SUCCESS(status)) {
 			RtlCopyMemory(Name, FileNameInfo->Name.Buffer, FileNameInfo->Name.MaximumLength);
-			KdPrint(("Create File. Filename = %ws \r\n", Name));
+			if (wcsncmp(Name, drive_filter.Buffer, wcslen(drive_filter.Buffer)) != 0) {
+				KdPrint(("%ws is filtered out. Does not match %ws. Returning back\n", Name, drive_filter.Buffer));
+				FltReleaseFileNameInformation(FileNameInfo);
+				return FLT_PREOP_SUCCESS_NO_CALLBACK;
+			}
+			TraceEvent(TRACE_INFO, "Create File. Filename = %ws \r\n", Name);
 		}
 		FltReleaseFileNameInformation(FileNameInfo);
 	}
@@ -74,6 +89,11 @@ FLT_PREOP_CALLBACK_STATUS MiniPreWrite(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OB
 		status = FltParseFileNameInformation(FileNameInfo);
 		if (NT_SUCCESS(status)) {
 			RtlCopyMemory(FileName, FileNameInfo->Name.Buffer, FileNameInfo->Name.MaximumLength);
+			if (wcsncmp(FileName, drive_filter.Buffer, wcslen(drive_filter.Buffer)) != 0) {
+				KdPrint(("%ws is filtered out. Does not match %ws. Returning back\n", FileName, drive_filter.Buffer));
+				FltReleaseFileNameInformation(FileNameInfo);
+				return FLT_PREOP_SUCCESS_NO_CALLBACK;
+			}
 
 			WCHAR UserName[1024] = { 0 };
 			PSecurityUserData SecData;
@@ -94,12 +114,100 @@ FLT_PREOP_CALLBACK_STATUS MiniPreWrite(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OB
 }
 
 
+NTSTATUS MiniConnect(PFLT_PORT clientport, PVOID serverportcookie, PVOID Context, ULONG size, PVOID *Connectioncookie) {
+	ClientPort = clientport;
+	TraceEvent(TRACE_VERBOSE, "Connect\r\n");
+	return STATUS_SUCCESS;
+}
+
+
+VOID MiniDisconnect(PVOID Connectioncookie) {
+	TraceEvent(TRACE_VERBOSE, "Disconnect\r\n");
+	FltCloseClientPort(FilterHandle, &ClientPort);
+}
+
+
+NTSTATUS MiniSendRecv(PVOID portcookie, PVOID InputBuffer, ULONG InputBufferLength, PVOID OutputBuffer, ULONG OutputBufferLength, PULONG RetLength) {
+	NTSTATUS status;
+	PCHAR msg = "KERNEL MSG";
+	TraceEvent(TRACE_INFO, "User msg: %s\r\n", (PCHAR)InputBuffer);
+	KdPrint(("User msg: %s\r\n", (PCHAR)InputBuffer));
+	strcpy((PCHAR)OutputBuffer, msg);
+	UNICODE_STRING TargetString;
+
+	TargetString.Buffer = (PWCH)ExAllocatePool(NonPagedPool, STRING_LEN * sizeof(WCHAR));
+	RtlZeroMemory(TargetString.Buffer, STRING_LEN * sizeof(WCHAR));
+	TargetString.MaximumLength = STRING_LEN;
+
+	if (strcmp((PCHAR)InputBuffer, "All drives") == 0) {
+		KdPrint(("ALL DRIVES\r\n"));
+	}
+	else {
+		KdPrint(("Individual Drives\r\n"));
+		wchar_t buf[STRING_LEN];
+		char inp_buf[STRING_LEN];
+		strcpy(inp_buf, "\\GLOBAL??\\");
+		strcat(inp_buf, (PCHAR)InputBuffer);
+		mbstowcs(buf, inp_buf, sizeof(inp_buf));
+		UNICODE_STRING symlinkName;
+		RtlInitUnicodeString(&symlinkName, buf);
+
+		OBJECT_ATTRIBUTES objectAttributes =
+			RTL_CONSTANT_OBJECT_ATTRIBUTES(&symlinkName,
+				OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE);
+
+		HANDLE symLinkHandle = NULL;
+		status = ZwOpenSymbolicLinkObject(&symLinkHandle,
+			SYMBOLIC_LINK_ALL_ACCESS,
+			&objectAttributes);
+
+		if (!NT_SUCCESS(status)) {
+			if (symLinkHandle)
+				ZwClose(symLinkHandle);
+			ExFreePool(TargetString.Buffer);
+			KdPrint(("ZwOpenSymbolicLinkObject failed\n"));
+			return status;
+		}
+
+		ULONG Ret = 0;
+
+		status = ZwQuerySymbolicLinkObject(symLinkHandle,
+			&TargetString, &Ret);
+		if (!NT_SUCCESS(status)) {
+			if (symLinkHandle)
+				ZwClose(symLinkHandle);
+			ExFreePool(TargetString.Buffer);
+			KdPrint(("ZwQuerySymbolicLinkObject failed\n"));
+			return status;
+		}
+		ZwClose(symLinkHandle);
+	}
+
+	KdPrint(("AFTER Query SymLink %S\r\n", TargetString.Buffer));
+	if (!wcsncmp(TargetString.Buffer, L"\\Device", 7)) {
+		status = STATUS_SUCCESS;
+		KdPrint(("Success: TargetStr has Device\n"));
+	}
+	RtlZeroMemory(drive_filter.Buffer, STRING_LEN * sizeof(WCHAR));
+	RtlUnicodeStringCopy(&drive_filter, &TargetString);
+	ExFreePool(TargetString.Buffer);
+	return STATUS_SUCCESS;
+}
+
 NTSTATUS MiniUnload(FLT_FILTER_UNLOAD_FLAGS Flags)
 {
 	UNREFERENCED_PARAMETER(Flags);
 	
-	KdPrint(("Driver Unload.\r\n"));
+	TraceEvent(TRACE_INFO, "Driver Unload.\r\n");
+	if (port) {
+		FltCloseCommunicationPort(port);
+		port = NULL;
+	}
 	FltUnregisterFilter(FilterHandle);
+	if (WppDriverObject)
+		WppCleanup(WppDriverObject);
+
+	ExFreePool(drive_filter.Buffer);
 	return STATUS_SUCCESS;
 }
 
@@ -107,18 +215,43 @@ extern "C"
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
 {
 	UNREFERENCED_PARAMETER(RegistryPath);
+	PSECURITY_DESCRIPTOR sd;
+	OBJECT_ATTRIBUTES oa = { 0 };
+	UNICODE_STRING name = RTL_CONSTANT_STRING(L"\\Mini-filter");
+	drive_filter.Buffer = (PWCH)ExAllocatePool(NonPagedPool, STRING_LEN * sizeof(WCHAR));
+	RtlZeroMemory(drive_filter.Buffer, STRING_LEN * sizeof(WCHAR));
+	drive_filter.MaximumLength = STRING_LEN;
+
+	WppDriverObject = DriverObject;
+	WppInit(WppDriverObject, RegistryPath);
 
 	NTSTATUS status;
 	status = FltRegisterFilter(DriverObject, &FilterRegistration, &FilterHandle);
 
+	if (!NT_SUCCESS(status))
+	{
+		ExFreePool(drive_filter.Buffer);
+		return status;
+	}
+	status = FltBuildDefaultSecurityDescriptor(&sd, FLT_PORT_ALL_ACCESS);
+
 	if (NT_SUCCESS(status))
 	{
-		status = FltStartFiltering(FilterHandle);
+		InitializeObjectAttributes(&oa, &name, OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE, NULL, sd);
+		status = FltCreateCommunicationPort(FilterHandle, &port, &oa, NULL, MiniConnect, MiniDisconnect, MiniSendRecv, 1);
 
-		if (!NT_SUCCESS(status))
-			FltUnregisterFilter(FilterHandle);
+		FltFreeSecurityDescriptor(sd);
+		if (NT_SUCCESS(status)) {
+			status = FltStartFiltering(FilterHandle);
+			if (NT_SUCCESS(status)) return status;	
+		}
+		if (port) {
+			FltCloseCommunicationPort(port);
+			port = NULL;
+		}
 	}
-
+	FltUnregisterFilter(FilterHandle);
+	ExFreePool(drive_filter.Buffer);
 	return status;
 
 }
